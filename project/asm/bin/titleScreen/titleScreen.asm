d81f33ad319d1ccdd1e005baefd3fe700b325a48
RobertTheSable <robertthesable@gmail.com>
2024-03-15 18:55:12 -0500

    Title screen documentation and attribution.
    - Add attribution to the title screen files.
    - Document and give descriptive name for each tileset part.

--- project/asm/bin/titleScreen/titleScreen.asm
+++ project/asm/bin/titleScreen/titleScreen.asm
@@ -1,9 +1,21 @@
+; The title screen elements here were created by Neill Corlett. 
+; It was created for this patch: https://www.romhacking.net/translations/356/
+; Please get permission from the original authors before reusing it.
+
+; All tilescreen components are compressed FE3's standard compression.
 
 ORG $de9359
-incbin title1.bin
+; "Fire Emblem" / "ファイアーエムブレム" tiles
+; These tiles are in 4BPP format
+incbin title_tiles.bin
 ORG $dea685
-incbin title2.bin
+; "Fire Emblem" / "ファイアーエムブレム" tilemap
+incbin title_tilemap.bin
 ORG $def044
-incbin title3.bin
+; "Mystery of the Emblem" / "紋章の謎" and Copyright Notice tiles
+; These tiles are in 2BPP format
+incbin subtitle_tiles.bin
 ORG $def6ff
-incbin title4.bin 
+; "FIREEMBLEM" and Sword tiles
+; These tiles are in 4BPP format
+incbin sword_etc_tiles.bin 
